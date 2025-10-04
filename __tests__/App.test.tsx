ebb3bf202871f7daa0aa07e71e081325d31f9f37
DongOk <hcan159@naver.com>
2025-10-04 16:16:49 +0900

    chore: reapply .gitignore and remove ignored files from tracking

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,7 +4,10 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../stopapp/App';
+// Make sure the path is correct and the file exists; if your App.tsx is in 'src' or the root, update the import accordingly.
+import App from '../App';
+// or, if it's in 'src':
+// import App from '../src/App';
 
 // Note: import explicitly to use the types shipped with jest.
 import {it} from '@jest/globals';
